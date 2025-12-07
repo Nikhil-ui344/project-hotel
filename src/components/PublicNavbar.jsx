36f18a87822fb773334964d63ec237021a0495ba
Nikhil-ui344 <nikhiln2999@.com>
2025-12-07 21:47:27 +0530

    navbar

--- src/components/PublicNavbar.jsx
+++ src/components/PublicNavbar.jsx
@@ -30,10 +30,10 @@ const PublicNavbar = () => {
         {/* Desktop Menu */}
         <div className="nav-links">
           <Link to="/" className="nav-link">Home</Link>
+          <Link to="/about" className="nav-link">About</Link>
           <Link to="/rooms" className="nav-link">Rooms</Link>
           <Link to="/gallery" className="nav-link">Gallery</Link>
           <Link to="/party-hall" className="nav-link">Party Hall</Link>
-          <Link to="/about" className="nav-link">About</Link>
           <Link to="/contact" className="nav-link">Contact</Link>
         </div>
 
@@ -48,10 +48,10 @@ const PublicNavbar = () => {
         {/* Mobile Menu Overlay */}
         <div className={`mobile-menu ${isOpen ? 'open' : ''}`}>
           <button className="mobile-nav-link" onClick={() => handleNavigation('/')}>Home</button>
+          <button className="mobile-nav-link" onClick={() => handleNavigation('/about')}>About</button>
           <button className="mobile-nav-link" onClick={() => handleNavigation('/rooms')}>Rooms</button>
           <button className="mobile-nav-link" onClick={() => handleNavigation('/gallery')}>Gallery</button>
           <button className="mobile-nav-link" onClick={() => handleNavigation('/party-hall')}>Party Hall</button>
-          <button className="mobile-nav-link" onClick={() => handleNavigation('/about')}>About</button>
           <button className="mobile-nav-link" onClick={() => handleNavigation('/contact')}>Contact</button>
         </div>
       </div>
